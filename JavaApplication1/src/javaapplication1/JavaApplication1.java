3872178f0cd80e2bf3db7c22165b61917757cdb3
u1 <u1@k7>
2016-03-14 17:13:10 +0100

    Ejemplos iniciales

--- JavaApplication1/src/javaapplication1/JavaApplication1.java
+++ JavaApplication1/src/javaapplication1/JavaApplication1.java
@@ -5,6 +5,8 @@
  */
 package javaapplication1;
 
+import java.sql.*;
+
 /**
  *
  * @author franlu
@@ -16,7 +18,52 @@ public class JavaApplication1 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        System.out.println("Probando");
+        System.out.println("Iniciando");
+        
+        // Creando la conexión
+        try {
+            Connection conexion = DriverManager.getConnection("jdbc:mysql://www.db4free.net:3306/izv_dawm","izv_dawm","123456"); 
+        }
+        catch ( Exception e){
+            e.printStackTrace();
+        }
+        
+        /**
+        public static void createEquipo(Connection con, String DBNombre) throws SQLException {
+            String createString = "create table " + DBNombre + ".EQUIPO " +
+                                    "(TEAM_ID integer NOT NULL," +
+                                    "PRIMARY KEY (TEAM_ID))";
+            
+            //Objeto para tirar sentencias a la DB
+            Statement stmt = null;
+            try {
+                stmt = con.createStatement();
+                stmt.executeUpdate(createString);
+            }
+            catch (SQLException e){
+                e.printStackTrace();
+            } finally {
+                stmt.close();
+            }
+        }
+        
+        public static void verEquipo(Connection con, String DBNombre) throws SQLException {
+            Statement stmt = null;
+            String query = "SELECT EQ_Nombre from " + DBNombre + ".EQUIPO";
+            try {
+                stmt = con.createStatement();
+                ResultSet rs = stmt.executeQuery(query);
+                while (rs.next()){
+                    String equipo = rs.getString("EQ_Nombre");
+                    
+                }
+            } catch (SQLException e) {
+                e.printStrackTrace();
+            } finally {
+                stmt.close();
+            }
+        
+        }**/
     }
     
 }
